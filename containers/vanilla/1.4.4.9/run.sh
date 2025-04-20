3447cffd3ede4857cd8ff24f63135044533092a0
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-20 18:18:26 -0400

    Initial Files

---
+++ containers/vanilla/1.4.4.9/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f "/config/serverconfig.txt" ]; then
+    cp ./serverconfig.default /config/serverconfig.txt
+fi
+
+if [ ! -f "/config/banlist.txt" ]; then
+    touch /config/banlist.txt
+fi
+
+screen -mS terra ./TerrariaServer -x64 -config /config/serverconfig.txt -banlist /config/banlist.txt $@
