b2fb5836fcd1a9f86b64d2ca3f37dfee697c8010
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-20 19:05:10 -0400

    Some scripts

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-podman push taytom259:vanilla-latest
+podman push taytom259/terraria:vanilla-latest
 #podman push taytom259:tshock-latest
 
 git add .
-git commit -m $1
+git commit -m "$1"
 git push
 
