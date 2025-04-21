a717ade410ce3dbc9d6fe4da3cbb77ffc2a0069d
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-21 09:45:35 -0400

    Scripted commit

--- containers/vanilla/1.4.4.9/run.sh
+++ containers/vanilla/1.4.4.9/run.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
 
-prep_term()
-{
-    unset term_child_pid
-    unset term_kill_needed
-    trap 'handle_term' TERM INT
-}
-
-handle_term()
-{
-    if [ "${term_child_pid}" ]; then
-	echo "sigterm caught"
-        #kill -TERM "${term_child_pid}" 2>/dev/null
-	screen -S terra -p 0 -X stuff "exit^M"
-    else
-        term_kill_needed="yes"
-    fi
-}
-
-wait_term()
-{
-    term_child_pid=$!
-    if [ "${term_kill_needed}" ]; then
-        kill -TERM "${term_child_pid}" 2>/dev/null 
-    fi
-    #wait ${term_child_pid} 2>/dev/null
-    wait
-    trap - TERM INT
-    #wait ${term_child_pid} 2>/dev/null
-    wait
-}
-
 if [ ! -f "/config/serverconfig.txt" ]; then
     cp ./serverconfig.default /config/serverconfig.txt
 fi
@@ -41,10 +10,10 @@ fi
 
 chmod -R g+w /config
 
-echo "Prep Term"
-prep_term
+trap 'touch /config/sigterm' SIGTERM
 
 screen -dmS terra ./TerrariaServer -x64 -config /config/serverconfig.txt -banlist /config/banlist.txt $@
 
-echo "Wait Term"
-wait_term
+until [ -e /config/sigterm  ]; do sleep 1; done
+rm /config/sigterm
+screen -S terra -p 0 -X stuff "exit^M"
