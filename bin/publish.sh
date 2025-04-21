28c3a8260fa4592a3257d270254a34403fd4e54e
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-20 20:21:14 -0400

    Scripted Commit

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+vlatest="1.4.4.9"
+rootdir=/home/ttomlin/development/terraria.container/
+
+cd $rootdir/containers/vanilla/1.4.4.9
+podman build -t "taytom259/terraria:vanilla-1.4.4.9" .
+
+podman tag "taytom259/terraria:vanilla-$vlatest" taytom259/terraria:vanilla-latest
+podman tag "taytom259/terraria:vanilla-$vlatest" taytom259/terraria:latest
+podman push taytom259/terraria:vanilla-1.4.4.9
 podman push taytom259/terraria:vanilla-latest
-#podman push taytom259:tshock-latest
+podman push taytom259/terraria:latest
+
+cd $rootdir
 
 git add .
-git commit -m "$1"
+if [ -z "$1" ]
+  then
+    git commit -m "Scripted Commit"
+  else
+    git commit -m "$1"
+fi
 git push
 
