50497e329197b1d86baff67beade61b7d6037cb4
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-21 11:41:29 -0400

    Scripted commit

--- containers/vanilla/1.4.4.9/watchdog.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sleep 3
-terraPID="$(screen -ls | grep -oE "[0-9]+\.terra" | sed -e "s/\..*$//g")"
-trap 'touch /config/sigterm' TERM
-until [ -e /config/sigterm ]; do sleep 1; done
-rm /config/sigterm
-screen -S terra -p 0 -X stuff "exit^M"
-wait $terraPID
