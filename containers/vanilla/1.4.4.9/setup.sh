462e3400db8d79e286647d9ff8cae3d8aa1554dd
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-21 15:25:34 -0400

    Scripted commit

--- containers/vanilla/1.4.4.9/setup.sh
+++ containers/vanilla/1.4.4.9/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+serverARGS="$@"
+
 if [ "$(id -u)" = 0 ]; then
   runAsUser=terraria
   runAsGroup=terraria
@@ -24,12 +26,39 @@ if [ "$(id -u)" = 0 ]; then
     fi
   fi
 
+  if [ ! -f "/config/serverconfig.txt" ]; then
+        cp ./serverconfig.default /config/serverconfig.txt
+  fi
+
+  if [ ! -f "/config/banlist.txt" ]; then
+        touch /config/banlist.txt
+  fi
+
+  if [ -n $WORLD  ]; then
+        serverARGS="-world /config/$WORLD $@"
+  fi
+
   chown -R ${runAsUser}:${runAsGroup} /config /opt/terraria
   chmod -R g+w /config
 
-  gosu ${runAsUser}:${runAsGroup} /opt/terraria/run.sh "$@"
+  #gosu ${runAsUser}:${runAsGroup} /opt/terraria/run.sh "$@"
+
+  date=$(date)
+  screen -dmS -L -Logfile /config/server.$date.log terra ./TerrariaServer -x64 -config /config/serverconfig.txt -banlist /config/banlist.txt "$serverARGS"
+
+  trap 'touch /root/sigterm' TERM
+  i=0
+  while [ ! -e /root/sigterm ]; do 
+	sleep 1
+	((i++))
+	if [ i -gt ((5*60)) ]; then
+		su -c 'screen -S terra -p 0 -X stuff 'save^M'' terraria
+		i=0
+	fi
+  done
+  su -c 'screen -S terra -p 0 -X stuff 'exit^M'' terraria
 
 else
-  echo "Setup permission not root! Please utilize ENV variables to set UID/GID."
-  exit 1
+	echo "Setup permission not root! Please utilize ENV variables to set UID/GID."
+	exit 1
 fi
