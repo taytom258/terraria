65abcf6cec92d7711f517ba7c63bd136fe31c7b8
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-20 22:10:56 -0400

    Scripted commit

--- containers/vanilla/1.4.4.9/run.sh
+++ containers/vanilla/1.4.4.9/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-EffUID="$(id -u)"
+EffUID=$(id -u)
 
 if [ $EffUID -eq 0 ]; then
 	useradd -m -s /bin/bash -k /etc/ske1/ -u $PUID terraria
 	chown -R terraria:terraria /opt/terraria /config
-	exec su terraria "$0" -- "$@"
-#else
-	#echo "Effective startup permissions are not root. Unable to setup proper permissions."
-	#exit 1
+	runuser -u terraria -- "$@"
+else
+	echo "Effective startup permissions are not root. Unable to setup proper permissions."
+	exit 1
 fi
 
 if [ ! -f "/config/serverconfig.txt" ]; then
