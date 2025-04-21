d51c73a747fcbf8392eae8cc0a2c139e5b737369
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-21 14:54:57 -0400

    Scripted commit

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,15 +2,24 @@
 
 vlatest="1.4.4.9"
 rootdir=/home/ttomlin/development/terraria.container/
+branch=$(git branch --showcurrent)
 
-cd $rootdir/containers/vanilla/1.4.4.9
-podman build -t "taytom259/terraria:vanilla-1.4.4.9" .
 
-podman tag "taytom259/terraria:vanilla-$vlatest" taytom259/terraria:vanilla-latest
-podman tag "taytom259/terraria:vanilla-$vlatest" taytom259/terraria:latest
-podman push taytom259/terraria:vanilla-1.4.4.9
-podman push taytom259/terraria:vanilla-latest
-podman push taytom259/terraria:latest
+if [ $branch -eq 'main' ]; then
+	cd $rootdir/containers/vanilla/1.4.4.9
+	podman build -t "taytom259/terraria:vanilla-1.4.4.9" .
+
+	podman tag "taytom259/terraria:vanilla-$vlatest" \
+		taytom259/terraria:vanilla-latest
+	podman tag "taytom259/terraria:vanilla-$vlatest" \
+		taytom259/terraria:latest
+	podman push docker.io/taytom259/terraria:vanilla-$vlatest
+	podman push docker.io/taytom259/terraria:vanilla-latest
+	podman push docker.io/taytom259/terraria:latest
+elif [ $branch -eq 'dev' ]; then
+	podman build -t "taytom259/terraria:vanilla-1.4.4.9-dev" .
+	podman push docker.io/taytom259/terraria:vanilla-$vlatest-dev
+fi
 
 cd $rootdir
 
