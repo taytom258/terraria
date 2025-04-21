bab698dffa4825063ccb22af9f0966eaf0f168dd
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-21 10:50:54 -0400

    Scripted commit

--- containers/vanilla/1.4.4.9/setup.sh
+++ containers/vanilla/1.4.4.9/setup.sh
@@ -27,7 +27,8 @@ if [ "$(id -u)" = 0 ]; then
   chown -R ${runAsUser}:${runAsGroup} /config /opt/terraria
   chmod -R g+w /config
 
-  gosu ${runAsUser} '/opt/terraria/run.sh'
+  gosu ${runAsUser}:${runAsGroup} '/opt/terraria/run.sh'
+  exit 0
 else
   echo "Setup permission not root! Please utilize ENV variables to set UID/GID."
   exit 1
