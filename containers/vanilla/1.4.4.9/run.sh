9e86b41ae0c215af7a2e4e282ba3142555cd2490
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-21 09:22:00 -0400

    Scripted commit

--- containers/vanilla/1.4.4.9/run.sh
+++ containers/vanilla/1.4.4.9/run.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+prep_term()
+{
+    unset term_child_pid
+    unset term_kill_needed
+    trap 'handle_term' TERM INT
+}
+
+handle_term()
+{
+    if [ "${term_child_pid}" ]; then
+	echo "sigterm caught"
+        #kill -TERM "${term_child_pid}" 2>/dev/null
+	screen -S terra -p 0 -X stuff "exit^M"
+    else
+        term_kill_needed="yes"
+    fi
+}
+
+wait_term()
+{
+    term_child_pid=$!
+    if [ "${term_kill_needed}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null 
+    fi
+    wait ${term_child_pid} 2>/dev/null
+    trap - TERM INT
+    wait ${term_child_pid} 2>/dev/null
+}
+
 if [ ! -f "/config/serverconfig.txt" ]; then
     cp ./serverconfig.default /config/serverconfig.txt
 fi
@@ -10,4 +39,8 @@ fi
 
 chmod -R g+w /config
 
-screen -dmS terra ./TerrariaServer -x64 -config /config/serverconfig.txt -banlist /config/banlist.txt $@
+prep_term
+
+screen -dmS terra ./TerrariaServer -x64 -config /config/serverconfig.txt -banlist /config/banlist.txt $@ &
+
+wait_term
