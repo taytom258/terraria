689b85ebb452500ceac2e64252a9d2bce347f702
taytom258 <3746203+taytom258@users.noreply.github.com>
2025-04-22 22:41:05 -0400

    Scripted commit

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,18 @@
 
 vlatest="1.4.4.9"
 
-podman build -t docker.io/taytom259/terraria:vanilla-$vlatest-dev .
-podman push docker.io/taytom259/terraria:vanilla-$vlatest-dev
+if [[ "$(git branch --show-current)" == "dev" ]]; then
+	podman build -t docker.io/taytom259/terraria:vanilla-$vlatest-dev .
+	podman push docker.io/taytom259/terraria:vanilla-$vlatest-dev
+else
+	podman build -t docker.io/taytom259/terraria:vanilla-$vlatest .
+	docker image tag docker.io/taytom259/terraria:vanilla-$vlatest docker.io/taytom259/terraria:latest
+	docker image tag docker.io/taytom259/terraria:vanilla-$vlatest docker.io/taytom259/terraria:vanilla-latest
+
+	podman push docker.io/taytom259/terraria:vanilla-$vlatest
+	podman push docker.io/taytom259/terraria:vanilla-latest
+	podman push docker.io/taytom259/terraria:latest
+fi
 
 git add .
 if [ -z "$1" ]
